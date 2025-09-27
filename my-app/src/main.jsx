d48bdd7577a48e3806fd22d69a7ed7a1526663c6
Timka09 <tymofij.i.naumenko@gmail.com>
2025-09-27 14:02:00 +0300

    jsx

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,9 +1,9 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 
-createRoot(document.getElementById('root'))
+createRoot(document.getElementById("root"))
   .render
   // <StrictMode>
   //   <App />
@@ -11,12 +11,52 @@ createRoot(document.getElementById('root'))
   ();
 
 const root = createRoot(document.getElementById("root"));
-  
+
 // Task2
-const myName = "Tima"
+const myName = "Tima";
 const rendName = <h1>{myName}</h1>;
 // Task3
 const welcome = <p>Ласкаво просимо</p>;
 // Task4
-// const alt = 
-// const photo = <img></img>;
+const image = React.createElement("img", {
+  src: "https://www.vetsulin.com/cats/wp-content/uploads/sites/180/2022/03/lead-cats-homepage-d-scaled.jpg",
+  alt: "kot bob",
+  width: 500,
+});
+// Task 5
+const favoriteWeb = {
+  name: "Youtube",
+  url: "https://www.youtube.com/",
+};
+
+const renderWeb = <a href={favoriteWeb.url}>{favoriteWeb.name}</a>;
+// Task 6
+const a = 10
+const b = 5
+
+const sum = <p>{a + b}</p>
+
+// Task 7
+const arr = ["Червоний", "Синій", "Зелений"];
+
+const arrFor = (
+  <ul>
+    {arr.map((color) => 
+      <li>
+        {color}
+    </li>
+    )}
+  </ul>
+)
+// render
+const rootAll = (
+  <div>
+    {rendName}
+    {welcome}
+    {image}
+    {renderWeb}
+    {sum}
+    {arrFor}
+  </div>
+);
+root.render(rootAll);
